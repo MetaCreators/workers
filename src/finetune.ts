a059152865d9cfefc488f771d677a4f9b3e8af89
Yash Hegde <yash4success@gmail.com>
2025-03-04 06:24:52 +0530

    todo

--- src/finetune.ts
+++ src/finetune.ts
@@ -26,33 +26,40 @@ export async function finetune(username: string,filename:string,userId:string) {
   console.log("presigned url for get zip file is", presignedGETURL);
    
   //once zip file task is done, integrate it here and test out training on replicate
-  // const training = await replicate.trainings.create(
-  //     "ostris",
-  //     "flux-dev-lora-trainer",
-  //     "e440909d3512c31646ee2e0c7d6f6f4923224863a6a10c494606e79fb5844497",
-  //     {
+  const training = await replicate.trainings.create(
+      "ostris",
+      "flux-dev-lora-trainer",
+      "e440909d3512c31646ee2e0c7d6f6f4923224863a6a10c494606e79fb5844497",
+      {
           
-  //         destination:  `adityaraj-007/${username}`,
-  //         input: {
-  //             steps: 1500,
-  //             lora_rank: 16,
-  //             optimizer: "adamw8bit",
-  //             batch_size: 1,
-  //             resolution: "512,768,1024",
-  //             autocaption: true,
-  //             //TODO: Fix input_images to accept zip file as input
-  //             input_images:presignedGETURL,
-  //             trigger_word: username,
-  //             learning_rate: 0.0004,
-  //             wandb_project: "flux_train_replicate",
-  //             autocaption_prefix: `photo of ${username}`,
-  //             wandb_save_interval: 100,
-  //             caption_dropout_rate: 0.05,
-  //             cache_latents_to_disk: false,
-  //             wandb_sample_interval: 100
-  //         }
-  //     }
-  // );
+          destination:  `adityaraj-007/${username}`,
+          input: {
+              steps: 1500,
+              lora_rank: 16,
+              optimizer: "adamw8bit",
+              batch_size: 1,
+              resolution: "512,768,1024",
+              autocaption: true,
+              input_images:presignedGETURL,
+              trigger_word: username,
+              learning_rate: 0.0004,
+              wandb_project: "flux_train_replicate",
+              autocaption_prefix: `photo of ${username}`,
+              wandb_save_interval: 100,
+              caption_dropout_rate: 0.05,
+              cache_latents_to_disk: false,
+              wandb_sample_interval: 100
+          }
+      }
+  );
+  //useful output parameters from training =>
+  //training.status
+  //completed_at
+  //created_at
+  //started_at
+  //id:
+  //training.input and training.output
+  //after training is completed, hit the backend ("/training-status" ROUTE) => in the backend, update the training status
 
   console.log("training output is: ",training);
   const outputVersion = training.output.version;
